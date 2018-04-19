c27701659b5b14936d08746c851e070d595de82f
BrendonPrado <36566914+BrendonPrado@users.noreply.github.com>
2018-04-18 22:22:43 -0300

    Add files via upload

---
+++ js/stackOver_bot.js
@@ -0,0 +1,59 @@
+const env = require('../.env')
+const telegraf = require('telegraf')
+const extra = require('telegraf/extra')
+const sqlite3 = require('sqlite3').verbose()
+const session = require('telegraf/session')
+const Markup = require('telegraf/markup')
+const bot = new telegraf(env.token)
+let db = new sqlite3.Database('./cont.db')
+  
+let sql = `Select titulo  tit,
+        cont con,
+        s_mais s
+from conteudo;`
+
+ops = []
+cont = []
+s_mais = []
+db.each(sql,(err,row)=>{
+    if(err){
+        console.log(err)
+    }
+    ops.push(`${row.tit}`) 
+    cont.push(`${row.con}`)
+    s_mais.push(`${row.s}`)
+})
+const botoes = () => extra.markup(Markup.inlineKeyboard(
+    ops.map(item=> Markup.callbackButton(item,`chama ${item}`)),
+    {columns:3}
+))
+bot.start(async c =>{
+    c.reply(`Bem vindo ${c.update.message.from.first_name} \n Escolha uma linguagem:`,botoes())
+})
+bot.action(/chama (.+)/,c =>{
+    const ob  = ops.findIndex(k =>k==c.match[1])
+    var i = 0
+    var ate =4000
+    var tam = cont[ob].length
+    while(ate <= tam){
+        c.reply(`${cont[ob].slice(i,ate)}`)
+        i+=4000
+        ate+=4000
+        if(ate > tam){
+            c .reply(`${cont[ob].slice(i,tam)}`)  
+         }
+  }
+    c.reply(`Quer Saber Mais?`,  Markup.keyboard([`Sim, quero saber mais sobre ${c.match[1]}`,'Não']).resize().oneTime().extra())
+    
+})
+bot.hears(/Sim, quero saber mais sobre (.+)/,c=>{
+    const ob  = ops.findIndex(k =>k==c.update.message.text.slice(28))
+    var i = 0
+    c.reply(`${s_mais[ob].slice(4096*i,4096*(i+1))}`,Markup.keyboard([`Continuar`]).resize().oneTime().extra())
+    i++
+})
+bot.on('text',c=>{
+    c.reply(`Escolha uma linguagem: ${c.update.message.from.first_name}`,botoes())
+
+})
+bot.startPolling()
